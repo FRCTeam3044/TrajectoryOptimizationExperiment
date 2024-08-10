d665bb07eb0d9740351d4a464051814f92a6811c
David DePauw <ddepauw3@gmail.com>
2024-08-10 12:09:54 -0400

    Initial Commit; waypoints, circular obstacles, approx dynamic constraints

---
+++ server/constraints.py
@@ -0,0 +1,91 @@
+import matplotlib.pylab as plt
+import math
+
+
+from dataclasses import dataclass, fields
+
+def constrainTo(problem, state, val):
+    for s, v in zip(state, val):
+        problem.subject_to(s==v)
+
+class Constraint():
+
+    def apply(self, problem, solution, start_ind, end_ind=None):
+        raise NotImplementedError
+
+class Waypoint(Constraint):
+    def __init__(self, **waypoint_dict) -> None:
+        super().__init__()
+        assert 'x' in waypoint_dict.keys()
+        assert 'y' in waypoint_dict.keys()
+
+        self.waypoint_dict = waypoint_dict
+
+        for k, v in self.waypoint_dict.items():
+            setattr(self, k, v)
+        
+    def hasValue(self, key):
+        return key in self.waypoint_dict.keys()
+    
+    def apply(self, problem, solution, start_ind):
+        prob_vars = [getattr(solution,k)[start_ind] for k in self.waypoint_dict.keys() if k != "theta" ]
+        vals = [v for k,v in self.waypoint_dict.items() if k != "theta"]
+        constrainTo(problem, prob_vars, vals)
+
+    def __repr__(self) -> str:
+        return "Waypoint: " + repr(self.waypoint_dict)
+    
+class FullRestWaypoint(Waypoint):
+    def __init__(self, **waypoint_dict) -> None:
+
+        waypoint_dict.update({"vx":0, "vy":0, "ax":0, "ay":0, "omega":0, "alpha":0})
+        super().__init__(**waypoint_dict)
+
+class InitialWaypoint(Waypoint):
+    def __init__(self, **waypoint_dict) -> None:
+        if 'theta' not in waypoint_dict.keys():
+            waypoint_dict['theta']=0
+        super().__init__(**waypoint_dict)
+
+class FullRestInitialWaypoint(FullRestWaypoint, InitialWaypoint):
+    def __init__(self, **waypoint_dict):
+        super().__init__( **waypoint_dict)
+
+
+class Obstacle(Constraint):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def plot(self, axis):
+        raise NotImplementedError
+    
+    def registerBot(self, botParams):
+        raise NotImplementedError
+
+class CircularObstacle(Obstacle):
+
+    def __init__(self, x, y, r) -> None:
+
+        self.x, self.y, self.r = x, y ,r
+        
+        super().__init__()
+    
+    def plot(self, axis):
+        axis.add_patch(plt.Circle((self.x, self.y), self.r,color='r'))
+
+    def registerBot(self, botParams):
+        edge = botParams.botEdgeSize
+        self.botRad = self.botRad = math.sqrt(edge*edge + edge*edge)/2
+
+    def apply(self, problem,solution, start_ind, end_ind=None):
+        x = solution.x[start_ind: end_ind]
+        y = solution.y[start_ind: end_ind]
+        if end_ind == None:
+            end_ind = start_ind+1
+
+        for xn, yn in zip(x,y):    
+            problem.subject_to((xn-self.x)*(xn-self.x) + (yn-self.y)*(yn-self.y) >= (self.r+self.botRad)* (self.r+self.botRad))
+
+
+    def __repr__(self) -> str:
+        return f"CircularObstacle(x = {self.x}, y = {self.y}, r = {self.r})"
---
+++ server/plotUtil.py
@@ -0,0 +1,100 @@
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.collections import LineCollection
+import matplotlib
+import math
+#https://matplotlib.org/stable/gallery/lines_bars_and_markers/multicolored_line.html
+def colored_line(x, y, c, ax, **lc_kwargs):
+    """
+    Plot a line with a color specified along the line by a third value.
+
+    It does this by creating a collection of line segments. Each line segment is
+    made up of two straight lines each connecting the current (x, y) point to the
+    midpoints of the lines connecting the current point with its two neighbors.
+    This creates a smooth line with no gaps between the line segments.
+
+    Parameters
+    ----------
+    x, y : array-like
+        The horizontal and vertical coordinates of the data points.
+    c : array-like
+        The color values, which should be the same size as x and y.
+    ax : Axes
+        Axis object on which to plot the colored line.
+    **lc_kwargs
+        Any additional arguments to pass to matplotlib.collections.LineCollection
+        constructor. This should not include the array keyword argument because
+        that is set to the color argument. If provided, it will be overridden.
+
+    Returns
+    -------
+    matplotlib.collections.LineCollection
+        The generated line collection representing the colored line.
+    """
+    if "array" in lc_kwargs:
+        warnings.warn('The provided "array" keyword argument will be overridden')
+
+    # Default the capstyle to butt so that the line segments smoothly line up
+    default_kwargs = {"capstyle": "butt"}
+    default_kwargs.update(lc_kwargs)
+
+    # Compute the midpoints of the line segments. Include the first and last points
+    # twice so we don't need any special syntax later to handle them.
+    x = np.asarray(x)
+    y = np.asarray(y)
+    x_midpts = np.hstack((x[0], 0.5 * (x[1:] + x[:-1]), x[-1]))
+    y_midpts = np.hstack((y[0], 0.5 * (y[1:] + y[:-1]), y[-1]))
+
+    # Determine the start, middle, and end coordinate pair of each line segment.
+    # Use the reshape to add an extra dimension so each pair of points is in its
+    # own list. Then concatenate them to create:
+    # [
+    #   [(x1_start, y1_start), (x1_mid, y1_mid), (x1_end, y1_end)],
+    #   [(x2_start, y2_start), (x2_mid, y2_mid), (x2_end, y2_end)],
+    #   ...
+    # ]
+    coord_start = np.column_stack((x_midpts[:-1], y_midpts[:-1]))[:, np.newaxis, :]
+    coord_mid = np.column_stack((x, y))[:, np.newaxis, :]
+    coord_end = np.column_stack((x_midpts[1:], y_midpts[1:]))[:, np.newaxis, :]
+    segments = np.concatenate((coord_start, coord_mid, coord_end), axis=1)
+
+    lc = LineCollection(segments, **default_kwargs)
+    lc.set_array(c)  # set the colors of each segment
+
+    return ax.add_collection(lc)
+
+def _plot_robot_trajectory(axis,fig, x, y, v):
+    lines = colored_line(x,y,v, axis)
+    fig.colorbar(lines)
+    
+
+def _plot_robot_rotation(axis, x, y, theta, botSize):
+    for xn,yn,t in zip(x,y,theta):
+        rect = matplotlib.patches.Rectangle((xn-botSize/2, yn-botSize/2), botSize, botSize, angle=math.degrees(t), rotation_point='center',facecolor='None', edgecolor='black')
+        axis.add_patch(rect)
+
+def _plot_obs(axis, obs):
+    for o in obs:
+        o.plot(axis)
+
+def plot_solution(probDef):
+    fig1, axis = plt.subplots()
+    # axis = plt.gca()
+
+    _plot_obs(axis, probDef.obs)
+
+    _plot_robot_rotation(axis, probDef.solution.x, probDef.solution.y, probDef.solution.theta, probDef.botParams.botEdgeSize)
+
+    _plot_robot_trajectory(axis, fig1, probDef.solution.x, probDef.solution.y, probDef.solution.v)
+    plt.axis('equal')
+
+    plt.show()
+
+def plot_value_graph(probDef, key):
+    plt.plot(probDef.solution.time, getattr(probDef.solution, key))
+    plt.title(key)
+    plt.show()
+
